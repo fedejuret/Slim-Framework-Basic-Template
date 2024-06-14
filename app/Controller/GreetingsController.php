9f8fbdac2482890f6eb5965a17d4f5726d6a3fe4
Federico Juretich <fedejuret@gmail.com>
2024-06-14 19:01:21 -0300

    Remove unused imports

--- app/Controller/GreetingsController.php
+++ app/Controller/GreetingsController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Jgut\Slim\Routing\Mapping\Attribute\Route;
-use App\Middleware\AuthorizationHeaderMiddleware;
-use Jgut\Slim\Routing\Mapping\Attribute\Middleware;
 
 class GreetingsController extends Controller
 {
