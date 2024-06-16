1bd3d4466533cb26988cea05b65ee3a47717cc70
Federico Juretich <fedejuret@gmail.com>
2024-06-16 02:40:13 -0300

    Add request validations

--- app/Controller/Controller.php
+++ app/Controller/Controller.php
@@ -5,9 +5,35 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Exceptions\ValidationException;
 
 abstract class Controller
 {
+    /**
+     * @throws \App\Exception\Exception\ValidationException
+     */
+    protected function validate(Request $request, array $rules): null|array|object
+    {
+        $data = $request->getParsedBody();
+        $request->getParsedBody()
+        $errors = [];
+
+        foreach ($rules as $field => $rule) {
+            try {
+                $rule->assert($data[$field] ?? null);
+            } catch (ValidationException $exception) {
+                $errors[$field] = $exception->getMessages();
+            }
+        }
+
+        if (! empty($errors)) {
+            throw new \App\Exception\Exception\ValidationException(json_encode($errors), 422);
+        }
+
+        return $data;
+    }
+
     protected function response(ResponseInterface $response, ?array $data = null, int $statusCode = 200): ResponseInterface
     {
         $responseInterface = $response->withHeader('Content-Type', 'application/json')->withStatus($statusCode);
--- app/Controller/GreetingsController.php
+++ app/Controller/GreetingsController.php
@@ -7,12 +7,17 @@ namespace App\Controller;
 use Jgut\Slim\Routing\Mapping\Attribute\Route;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Respect\Validation\Validator;
 
 final class GreetingsController extends Controller
 {
     #[Route(pattern: '/greeting/{name}')]
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $args): ResponseInterface
     {
+        $this->validate($request, [
+            'name' => Validator::exists()->stringType()->length(5, 10),
+        ]);
+
         return $this->response($response, [
             'status' => 'Hi, ' . $args['name'],
         ]);
